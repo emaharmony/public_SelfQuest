37f0e1ebcc24e5c73b8a9e6752d9497059368a88
EMA <emaharmony@gmail.com>
2021-07-01 14:28:12 -0400

    Chatacter Creation Screen Prototype
    I set up the scene. Looks Cute af

---
+++ Assets/PulsateLight.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class PulsateLight : MonoBehaviour
+{
+    Light light;
+    float timer;
+    bool direction = true;
+
+
+    [SerializeField] float maxIntensity, minIntensity, maxRange, minRange, pingpongSpeed = 1;
+    [SerializeField] bool doIntensity, doRange;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        light = GetComponent<Light>();
+    }
+
+   // Update is called once per frame
+    void Update()
+    {
+
+        if (doRange)
+            light.range = PingPong(light.range, maxRange, minRange); ;
+
+        if (doIntensity)
+            light.intensity = PingPong(light.intensity, maxIntensity, minIntensity);
+
+        timer += Time.deltaTime;
+    }
+
+    float PingPong(float val, float max, float min) 
+    {
+        float result = val;
+        if (direction)
+        {
+            if (result < max)
+                result += (Time.deltaTime * pingpongSpeed);
+            else
+                direction = false;
+        }
+        else 
+        {
+            if (result > min)
+                result -= (Time.deltaTime * pingpongSpeed);
+            else
+                direction = true;
+        }
+
+        return result;
+    }
+}
